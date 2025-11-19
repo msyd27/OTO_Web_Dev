ea8d44418153b211d19e255f14d72d113df3aad2
Zayd Qadri <61556844+Zaydqadri@users.noreply.github.com>
2025-11-18 23:07:40 -0500

    about

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -9,7 +9,18 @@
           Across the region, communities consistently seek knowledgeable Ulamaa, Imams, and teachers to serve in essential roles—teaching in Madaris, delivering Jumu‘ah khutbahs, conducting programs, leading prayers, and offering spiritual mentorship.<br /><br />
 
           Our goal is to support the growth and stability of Islamic institutions by ensuring they are empowered with dedicated individuals who can enrich their programs and spiritual services.</p>
-  
+        <br></br>
+        <h1 className="text-3xl font-bold text-[var(--brand)]">Why We Exist</h1>
+
+<p>Ontario Taraweeh Outreach was created to bridge an important gap in our communities by:</p>
+ <ul className="list-disc list-outside text-black/80 pl-4">
+        <li>Connecting qualified Huffadh, Imams, Ulamaa, and Qur’an teachers with Masajid, Musallahs, and Madaris through a simple and transparent platform</li>
+        <li>Helping institutions find reliable individuals while giving service-minded individuals access to meaningful opportunities</li>
+        <li>Strengthening and uniting the Muslim community by improving access to strong religious leadership and dedicated Huffadh of the Quraan</li>
+        <li>Enriching community life and supporting a more spiritually connected environment across Ontario and beyond</li>
+</ul>
+<br></br>
+
        <h1 className="text-3xl font-bold text-[var(--brand)]">Our Vision</h1>
        <p className="mt-4 text-[var(--ink)]/80">
        We envision a region where every Masjid, Musallah, and Madrasah can find the right people at the right time with ease, and where every Hafidh, Imam, and scholar has access to opportunities that help them teach, lead, and grow. By fostering these connections, we hope to strengthen the spiritual fabric of our communities and promote excellence in Islamic service throughout Canada and beyond.</p>
