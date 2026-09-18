8e8aab4c23b59470bef7167e5fae397fa6f20f3c
Yusuf Syed <yusuf.ar.syed@gmail.com>
2026-09-17 23:11:04 -0400

    created resources page

---
+++ src/pages/resources.tsx
@@ -0,0 +1,16 @@
+import Head from "next/head";
+
+export default function ResourcesPage() {
+  return (
+    <>
+      <Head>
+        <title>Resources - Ontario Taraweeh Outreach</title>
+      </Head>
+      <section className="mx-auto max-w-4xl px-4 py-12">
+        <h1 className="text-3xl font-bold text-[var(--brand)] mb-6">
+          Resources
+        </h1>
+      </section>
+    </>
+  );
+}
